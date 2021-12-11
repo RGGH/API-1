344b5b89d8608322bb72b4b3cc9e898fc9dfd99c
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-12-11 22:45:44 +0000

    Add files via upload

---
+++ Full_Project/test_1.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://neuro-2fle3wxkia-nw.a.run.app/vars"
+
+params = {
+  "weeks": 6,
+  "years": 0,
+  "books": 0,
+  "projects": 0,
+  "earn": 0
+}
+
+answer = requests.post(url, json = params)
+
+print(answer.text)
